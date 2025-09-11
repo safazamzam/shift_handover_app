ce1f9d4324f21c186bb948b98741b7e813d41580
safazamzam <mdsafazamzam@gmail.com>
2025-09-11 15:47:17 +0530

    Update misc.py

--- routes/misc.py
+++ routes/misc.py
@@ -1,28 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from services.audit_service import log_action
 
 misc_bp = Blueprint('misc', __name__)
 
 @misc_bp.route('/change-management')
 def change_management():
+    log_action('View Change Management Tab', f'Path: {request.path}')
     return render_template('coming_soon.html', title='Change Management')
 
 @misc_bp.route('/problem-tickets')
 def problem_tickets():
+    log_action('View Problem Tickets Tab', f'Path: {request.path}')
     return render_template('coming_soon.html', title='Problem Tickets')
 
 @misc_bp.route('/kb-details')
 def kb_details():
+    log_action('View KB Details Tab', f'Path: {request.path}')
     return render_template('coming_soon.html', title='KB Details')
 
-@misc_bp.route('/vendor-details')
-def vendor_details():
-    return render_template('coming_soon.html', title='Vendor Details')
 
 @misc_bp.route('/postmortem-details')
 def postmortem_details():
+    log_action('View Postmortem Details Tab', f'Path: {request.path}')
     return render_template('coming_soon.html', title='Postmortem Details')
 
-@misc_bp.route('/application-details')
-def application_details():
-    return render_template('coming_soon.html', title='Application Details')
 
